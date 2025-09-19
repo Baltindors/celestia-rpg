81f84cbf14bca508d968de709de58a053f28dd1b
Baltindors <leosan3.141@gmail.com>
2025-09-18 21:36:12 -0400

    home changes

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <h1>Home</h1>
-    <div v-if="user">
-      <p>Welcome, {{ user.name }}</p>
+    <div v-if="user && user.race">
+      <h1>Game Dashboard</h1>
+      <p>Welcome back, {{ user.name }} of the {{ user.race }}!</p>
       <button @click="handleLogout">Logout</button>
     </div>
-    <div v-else>
+
+    <div v-else-if="isLoading">
+      <p>Loading your profile...</p>
+    </div>
+
+    <div v-else-if="!user">
+      <h1>Home</h1>
       <p>You are not logged in.</p>
       <router-link to="/login">Login</router-link>
     </div>
@@ -13,30 +19,51 @@
 </template>
 
 <script>
-import { onMounted, computed } from "vue";
-import { useRouter } from "vue-router"; // Import useRouter
+import { onMounted, computed, ref, watch } from "vue";
+import { useRouter } from "vue-router";
 import { useAuthStore } from "../store/auth";
 
 export default {
   setup() {
     const authStore = useAuthStore();
-    const router = useRouter(); // Initialize router
+    const router = useRouter();
+    const isLoading = ref(true);
 
-    onMounted(() => {
-      if (!authStore.user) {
-        authStore.fetchUser();
+    const user = computed(() => authStore.user);
+
+    const checkUserRace = (currentUser) => {
+      if (currentUser) {
+        // If user is logged in but has NOT selected a race, redirect
+        if (!currentUser.race) {
+          router.push("/race-selection");
+        }
       }
+      // If we have a user (with a race) or no user at all, stop loading
+      isLoading.value = false;
+    };
+
+    onMounted(async () => {
+      // If we don't have user data, fetch it
+      if (!authStore.user) {
+        await authStore.fetchUser();
+      }
+      checkUserRace(authStore.user);
     });
 
-    // Logout handler
+    // Watch for changes in the user object (e.g., after login)
+    watch(user, (newUser) => {
+      checkUserRace(newUser);
+    });
+
     const handleLogout = async () => {
       await authStore.logout();
-      router.push("/login"); // Redirect to login page after logout
+      router.push("/login");
     };
 
     return {
-      user: computed(() => authStore.user),
-      handleLogout, // Expose handler to the template
+      user,
+      isLoading,
+      handleLogout,
     };
   },
 };
